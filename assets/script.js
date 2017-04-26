c7e18d2cadcd068ebe175b6758287d3b9657e673
isabelllacer <isabelllacer@Isabels-MacBook-Pro-2.local>
2017-04-25 18:30:21 -0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,122 @@
+$(document).ready(function(){
+	$(".home-isabel").addClass("home-isabelt");
+	$(".home-llacer").addClass("home-llacert");
+
+	$(".home-button").click(function(event) {
+		$(".sidebar").addClass("sidebar-show");
+		event.stopPropagation();
+
+		$(".sidebar").click(function(event) {
+			event.stopPropagation();
+		});
+
+		$(window).scroll(function () {
+		 	var height = $(window).scrollTop();
+		 	if (height >= 768) {
+		 		if ($(".sidebar").hasClass("sidebar-show")) {
+		 			$(".sidebar").removeClass("sidebar-show");
+		 		}
+		 	}
+		});
+
+		$(".x-button").click(function() {
+				$(".sidebar").removeClass("sidebar-show");
+		});
+
+		$(".home-container").click(function() {
+				$(".sidebar").removeClass("sidebar-show");
+		});
+	});
+
+	$(".sidebar-about").hover(function() {
+		$(".underline1").addClass("sidebar-show");
+	}, function() {
+		$(".underline1").removeClass("sidebar-show");
+	});
+
+	$(".sidebar-work").hover(function() {
+		$(".underline2").addClass("sidebar-show");
+	}, function() {
+		$(".underline2").removeClass("sidebar-show");
+	});
+
+	$(".sidebar-contact").hover(function() {
+		$(".underline3").addClass("sidebar-show");
+	}, function() {
+		$(".underline3").removeClass("sidebar-show");
+	});
+
+	$(".sidebar-about").click(function() {
+		$(window).scrollTop(768);
+	});
+	$(".sidebar-work").click(function() {
+		$(window).scrollTop(1306);
+	});
+	$(".sidebar-contact").click(function() {
+		$(window).scrollTop(4356);
+	});
+
+
+
+	$(window).scroll(function () {
+		var height = $(window).scrollTop();
+		if (height < 768) {
+			$(".navbar-line1").removeClass("navbar-line1-active");
+			$(".navbar").removeClass("navbar-fixed");
+			$(".logo-container").removeClass("navbar-fixed");
+		}
+		if (height >= 668 && height <= 1306) {
+			$(".navbar-line1").addClass("navbar-line1-active");
+			$(".navbar-line2").removeClass("navbar-line2-active");
+		}
+		if (height >= 768) {
+			$(".navbar").addClass("navbar-fixed");
+			$(".logo-container").addClass("navbar-fixed");
+		}
+		if (height >= 1006 && height < 4056) {
+			$(".navbar-line1").addClass("background-white");
+			$(".navbar-about").addClass("white");
+			$(".navbar-work").addClass("white");
+			$(".navbar-contact").addClass("white");
+			$(".navbar-line2").removeClass("background-black");
+			$(".about-logo-white").addClass("opacity-one");
+		}
+		if (height < 1006) {
+			$(".navbar-line1").removeClass("background-white");
+			$(".navbar-about").removeClass("white");
+			$(".navbar-work").removeClass("white");
+			$(".navbar-contact").removeClass("white");
+			$(".about-logo-white").removeClass("opacity-one");
+		}
+		if (height >= 4056) {
+			$(".navbar-about").removeClass("white");
+			$(".navbar-work").removeClass("white");
+			$(".navbar-contact").removeClass("white");
+			$(".navbar-line2").addClass("background-black");
+			$(".about-logo-white").removeClass("opacity-one");
+		}
+		if (height >= 1206 && height < 4256) {
+			$(".navbar-line1").removeClass("navbar-line1-active");
+			$(".navbar-line2").addClass("navbar-line2-active");
+			$(".navbar-line3").removeClass("navbar-line3-active");
+		}
+
+		if (height > 4256) {
+			$(".navbar-line2").removeClass("navbar-line2-active");
+			$(".navbar-line3").addClass("navbar-line3-active");
+		}
+	});
+
+	$(".navbar-about").click(function() {
+		$(window).scrollTop(768);
+	});
+	$(".navbar-work").click(function() {
+		$(window).scrollTop(1306);
+	});
+	$(".navbar-contact").click(function() {
+		$(window).scrollTop(4356);
+	});
+
+
+
+});
